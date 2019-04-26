fb4d7e833004bedc02f0c6584165d74ba77c6be0
Dmitry Dvorov <d.dvorov@yandex.ru>
2019-04-26 22:48:31 +0300

    #3 Add UpToTop button

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 
 $(document).ready(function($){
-   $(this).on('click', 'a[href^="#"]', function () {
+
+
+    $(this).on('click', 'a[href^="#"]', function () {
         $('html, body').animate({
-             scrollTop:  $('a[name="'+this.hash.slice(1)+'"]').offset().top 
-            }, 1000 );
+            scrollTop:  $('a[name="'+this.hash.slice(1)+'"]').offset().top 
+        }, 1000 );
         return false;
     });
-});
 
+    $('body').append('<a href="#" id="go-top" title="Вверх">Вверх</a>');
+
+    $.fn.scrollToTop = function() {
+        $(this).hide().removeAttr("href");
+        if ($(window).scrollTop() >= "250") $(this).fadeIn("slow");
+        var scrollDiv = $(this);
+        $(window).scroll(function() {
+            if ($(window).scrollTop() <= "250") {
+                $(scrollDiv).fadeOut("slow");
+            } else $(scrollDiv).fadeIn("slow");
+        });
+        $(this).click(function() {
+            $("html, body").animate({scrollTop: 0}, "slow")
+        });
+    };
+    $("#go-top").scrollToTop();
+    
+});
+   
 
 
